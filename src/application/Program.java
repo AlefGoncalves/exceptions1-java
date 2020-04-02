c5ae685a7e377a86c337352dac60e59e88715771
AlefGoncalves <alefgoncalvesrios@gmail.com>
2020-04-02 19:15:37 -0300

    Good Solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.awt.HeadlessException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,31 +7,34 @@ import java.util.Date;
 import javax.swing.JOptionPane;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws HeadlessException, ParseException {
+	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		int number = Integer.parseInt(JOptionPane.showInputDialog(null, "Room Number: "));
-		Date checkin = sdf.parse(JOptionPane.showInputDialog(null, "Check-in date (dd/MM/yyyy): "));
-		Date checkout = sdf.parse(JOptionPane.showInputDialog(null, "Check-out date (dd/MM/yyyy): "));
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			int number = Integer.parseInt(JOptionPane.showInputDialog(null, "Room Number: "));
+			Date checkin = sdf.parse(JOptionPane.showInputDialog(null, "Check-in date (dd/MM/yyyy): "));
+			Date checkout = sdf.parse(JOptionPane.showInputDialog(null, "Check-out date (dd/MM/yyyy): "));
 
-		if (!checkout.after(checkin)) {
-			JOptionPane.showMessageDialog(null, "Error in reservation!!");
-		} else {
 			Reservation reservation = new Reservation(number, checkin, checkout);
 			JOptionPane.showMessageDialog(null, "Reservation : " + reservation);
 			checkin = sdf.parse(JOptionPane.showInputDialog(null, "Check-in date (dd/MM/yyyy): "));
 			checkout = sdf.parse(JOptionPane.showInputDialog(null, "Check-out date (dd/MM/yyyy): "));
 
-			String error = reservation.updateDates(checkin, checkout);
-			if (error != null) {
-				JOptionPane.showMessageDialog(null, error);
-			} else {
-				JOptionPane.showMessageDialog(null, "Reservation : " + reservation);
-			}
+			reservation.updateDates(checkin, checkout);
+			JOptionPane.showMessageDialog(null, "Reservation : " + reservation);
+
+		} catch (ParseException e) {
+			JOptionPane.showMessageDialog(null, "Invalid Format");
+		} catch (DomainException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		} catch (RuntimeException e) {
+			JOptionPane.showMessageDialog(null, "Unexpected Error!!");
 		}
+
 	}
 
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,18 +4,25 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	private Integer roomNumber;
 	private Date checkin;
 	private Date checkout;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
-	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
-	
+
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) throws DomainException {
+
+		if (!checkout.after(checkin)) {
+			throw new DomainException("Error in reservation!!");
+		}
+		
 		this.roomNumber = roomNumber;
 		this.checkin = checkin;
 		this.checkout = checkout;
+
 	}
 
 	public Integer getRoomNumber() {
@@ -38,31 +45,23 @@ public class Reservation {
 		long diff = checkout.getTime() - checkin.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public String updateDates(Date checkin, Date checkout) {
-		
+
+	public void updateDates(Date checkin, Date checkout) throws DomainException {
+
 		Date now = new Date();
 		if (checkin.before(now) || checkout.before(now)) {
-			return "Error in reservation!! Invalid Date";
+			throw new DomainException("Error in reservation!! Invalid Date");
 		} else if (!checkout.after(checkin)) {
-			return "Error in reservation!!";
+			throw new DomainException("Error in reservation!!");
 		}
-		
+
 		this.checkin = checkin;
 		this.checkout = checkout;
-		return null;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Room "
-				+ roomNumber
-				+ ", check-in: "
-				+ sdf.format(checkin)
-				+ ", check-out: "
-				+ sdf.format(checkout)
-				+ ", "
-				+ duration()
-				+ " nights";
+		return "Room " + roomNumber + ", check-in: " + sdf.format(checkin) + ", check-out: " + sdf.format(checkout)
+				+ ", " + duration() + " nights";
 	}
 }
